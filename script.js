208f0a730110afd4823bdd1176d1054a8bd24cbc
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-01-26 20:22:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-              
 var functionu
+
 {
-document.addEventlistener(onclick());
+document.addEventlistener(onclick);
 }
 onclick()
 {
 console.log ("tr",${vart/vart});
 console.log("tr",$vart);
+console.log("tr",$vart);
+console.log("td", ${gameuser});
+console.log("tr", ${});
 }
 }
