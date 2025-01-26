dbcab0938686d4d4016307a5e2bdc374d81f0672
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-01-26 20:28:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.addEventlistener(onclick);
 }
 onclick()
 {
-console.log ("tr",${vart/vart});
-console.log("tr",$vart);
-console.log("tr",$vart);
-console.log("td", ${gameuser});
-console.log("tr", ${});
+console.log ("tr",${wvart/vart});
+console.log("tr",$(vart);
+console.log("tr",$(gamers);
+console.log("td", ${bonuses});
+console.log("tr", ${events});
 }
 }
