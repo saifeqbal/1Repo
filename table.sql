a02e6b6fcc7018e1c60c895ab721e17df1478b83
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-01-28 16:21:23 +0530

    Update

---
+++ table.sql
@@ -0,0 +1,5 @@
+create database tablen
+create table tablen;
+insert column ["gamers table", varchar(" 256") , values (" mech arena")];
+insert column[ "title" varchar  ("32") values("Galaxiaga"),("OS, Android")];
+insert column ["Release " varchar("54") ,values( ("12/54")];
