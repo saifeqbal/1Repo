d9f9ca003dce2c736a58bcf93fe93ae0383bc103
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-01-29 14:18:02 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,9 +1,9 @@
 create database tablen
 create table tablen;
 insert column ["gamers table", varchar(" 256") ,where values ("gamers table"),(" game starts") ("scores")  ("players") ];
-insert column[ "title" , varchar ("32") values [(" OS, Android" ) , (Street fighter" ),(tekken"), (" Ronin" )("call of dragons "),("OS, Android")];
+insert column[ "title" , varchar ("32") values [(" OS, Android" )]
+ , ("Street fighter" ),(tekken"), (" Ronin" )("call of dragons "),("OS, Android")];
 insert column ["Release " varchar("54"),values( ("12/54")];
-
 insert row("game 1", (32) values ("Street fighter")];
 INSERT ROW ["GAME 2" VARCHAR 16 VALUES("call of dragons")];
 INSERT row[ "game 3",VARCHAR 16, values("Galaxiaga")];
