4f3896c7603d640a606270de17807d09dd20b055
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-01-29 14:13:33 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,11 +1,13 @@
 create database tablen
 create table tablen;
-insert column ["gamers table", varchar(" 256") , values (" gamers table")];
-insert column[ "title" , varchar ("32") values("Title") (" ") ,("OS, Android")];
+insert column ["gamers table", varchar(" 256") ,where values ("gamers table"),(" game starts") ("scores")  ("players") ];
+insert column[ "title" , varchar ("32") values [(" OS, Android" ) , (Street fighter" ),(tekken"), (" Ronin" )("call of dragons "),("OS, Android")];
 insert column ["Release " varchar("54") ,values( ("12/54")];
-insert row("game 1", varchar("32") values ("Street fighter")]
+
+insert row("game 1", (32) values ("Street fighter")];
 INSERT ROW ["GAME 2" VARCHAR 16 VALUES("call of dragons")];
-INSERT 
-
-
-
+INSERT row[ "game 3",VARCHAR 16, values("Galaxiaga")];
+insert row ["game 4", VARCHAR 16 , VALUES("MECH ARENA");
+INSERT ROW ["game 5", VARCHAR 16 , VALUES("BULLET ECHO INDIA"),("")];
+INSERT ROW [ GAME 5 VARCHAR 16 VALUES [("BATTLEGROUNDS INDIA") ("")];
+insert into column  where values [()];
