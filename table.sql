eeddb02f01ddb941fc28d2e939cc82078521d6ec
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-01-29 14:27:29 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -4,10 +4,39 @@ insert column ["gamers table", varchar(" 256") ,where values ("gamers table"),("
 insert column[ "title" , varchar ("32") values [(" OS, Android" )]
  , ("Street fighter" ),(tekken"), (" Ronin" )("call of dragons "),("OS, Android")];
 insert column ["Release " varchar("54"),values( ("12/54")];
+INSERT INTO COLUMN [("GAMES")];
+{
+
 insert row("game 1", (32) values ("Street fighter")];
 INSERT ROW ["GAME 2" VARCHAR 16 VALUES("call of dragons")];
 INSERT row[ "game 3",VARCHAR 16, values("Galaxiaga")];
 insert row ["game 4", VARCHAR 16 , VALUES("MECH ARENA");
 INSERT ROW ["game 5", VARCHAR 16 , VALUES("BULLET ECHO INDIA"),("")];
-INSERT ROW [ GAME 5 VARCHAR 16 VALUES [("BATTLEGROUNDS INDIA") ("")];
-insert into column() where values [()];
+INSERT ROW [ "GAME 6" VARCHAR 16 VALUES [("BATTLEGROUNDS INDIA") ("")];
+insert into column  [("games") ,values [("")];
+
+
+-- Create a SQL file named 'image_storage.sql'
+-- Create the table for storing pictures
+CREATE TABLE Pictures (
+    PictureID INT AUTO_INCREMENT PRIMARY KEY,
+    FileName VARCHAR(255) NOT NULL,
+    FileType VARCHAR(50) NOT NULL,
+    FileSize INT NOT NULL,
+    ImageData BLOB NOT NULL,
+    UploadDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- Example of how to insert an image into the table
+-- Note: Here, '?' is used as a placeholder for prepared statements in many SQL dialects
+-- You would replace '?' with your actual data or use parameters in your application
+INSERT INTO Pictures (FileName, FileType, FileSize, ImageData) 
+VALUES (?, ?, ?, ?);
+-- Example to retrieve an image
+SELECT ImageData FROM Pictures WHERE PictureID = ?;
+-- Example to update an image
+UPDATE Pictures 
+SET FileName = ?, FileType = ?, FileSize = ?, ImageData = ? 
+WHERE PictureID = ?;
+
+-- Example to delete an image
+DELETE FROM Pictures WHERE PictureID = ?;
