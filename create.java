6397adc993deea93f6890eee79a4f2418ada47f0
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-07 17:24:51 +0530

    Update create.java

--- create.java
+++ create.java
@@ -1,5 +1,17 @@
 import java.io.*;
 import java.awt.*;
-<> 
+import java.io.*;
+import java.lang.html;
+import java.sql.connction
+{
+import SQL.connection;
+import java.connection.sql;
+username
+{
+"saif", "eqbal", 
+"java code", 
+};
+login : {"nousename"}
+password: {"#1alightning"}
+}
 
-<>
