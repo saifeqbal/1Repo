a6af13df88304c49147d5df54844c68a9c9c90a2
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-09 01:34:35 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,10 +1,10 @@
 create database tablen;
 create table tablen{
 
-insert column ["gamers table", varchar(" 256") ,where values ("gamers table"),(" game starts") ("scores")  ("players") ];
+insert column ["gamers table", varchar(" 256") ,where values ("gamers table"),(" game starts") ("scores")  ("players")];
 insert column[ "title" , varchar ("32") values [(" OS, Android" )];
- insert column ("games") VARCHAR 64 WHERE values [ ("Street fighter" ),("tekken"), (" Ronin" )("call of dragons "),("OS, Android")];
-insert column ["Release " varchar("54"),values( ("12/54")];
+ insert column ("games") VARCHAR 64 WHERE values [ ("Street fighter" ),("tekken"),(" Ronin" ),("call of dragons "),("OS, Android")]; 
+insert column ["Release" varchar("54"),values( ("12/54" )];
 INSERT INTO COLUMN [("Release")];
 {
 values("")
@@ -12,13 +12,12 @@ values("")
 insert row("GAME 1", (32) values ("Street fighter")];
 INSERT ROW ["GAME 2" VARCHAR 16 VALUES("call of dragons")];
 INSERT row[ "game 3",VARCHAR 16, values("Galaxiaga")];
-insert row ["game 4", VARCHAR 16 , VALUES("MECH ARENA");
+insert row ["game 4",VARCHAR 16 , VALUES("MECH ARENA");
 INSERT ROW ["game 5", VARCHAR 16 , VALUES("BULLET ECHO INDIA"),("")];
 INSERT ROW [ "GAME 6" VARCHAR 16 VALUES [("BATTLEGROUNDS INDIA") ("23/10")];
 insert into column  [("games") ,values [("vintage games")];
 }
-
-
+insert into column[("stats"),values ("1")];
 -- Create a SQL file named 'image_storage.sql'
 -- Create the table for storing pictures
 CREATE TABLE Pictures (
@@ -29,6 +28,7 @@ CREATE TABLE Pictures (
     ImageData BLOB NOT NULL,
     UploadDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 -- Example of how to insert an image into the table
 -- Note: Here, '?' is used as a placeholder for prepared statements in many SQL dialects
 -- You would replace '?' with your actual data or use parameters in your application
