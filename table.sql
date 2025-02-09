f7c9541dbd7412318f3925f8d2614884d53ad941
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-09 23:00:10 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,23 +1,36 @@
 create database tablen;
-create table tablen{
-
-insert column ["gamers table", varchar(" 256") ,where values ("gamers table"),(" game starts") ("scores")  ("players")];
-insert column[ "title" , varchar ("32") values [(" OS, Android" )];
- insert column ("games") VARCHAR 64 WHERE values [ ("Street fighter" ),("tekken"),(" Ronin" ),("call of dragons "),("OS, Android")]; 
-insert column ["Release" varchar("54"),values( ("12/54" )];
-INSERT INTO COLUMN [("Release")];
+create table tablen
 {
-values("")
+insert column ["gamers table", 
+varchar(" 256") ,where values ("gamers table"),(" game starts") ("scores")  ("players")];
+
+insert column[ "title" , varchar ("32") values [(" OS, Android" )];
+
+ insert column ("games") VARCHAR 64 WHERE values [ ("Street fighter"),("tekken"),(" Ronin"),("call of dragons"),("OS, Android")]; 
+insert column ["Release" varchar("54"),values( ("12/54")("12,24")];
+
+INSERT INTO COLUMN [("Release")];
+
+{
+values("12/2021","12/2022","12/2023");
 }
 insert row("GAME 1", (32) values ("Street fighter")];
 INSERT ROW ["GAME 2" VARCHAR 16 VALUES("call of dragons")];
 INSERT row[ "game 3",VARCHAR 16, values("Galaxiaga")];
 insert row ["game 4",VARCHAR 16 , VALUES("MECH ARENA");
-INSERT ROW ["game 5", VARCHAR 16 , VALUES("BULLET ECHO INDIA"),("")];
-INSERT ROW [ "GAME 6" VARCHAR 16 VALUES [("BATTLEGROUNDS INDIA") ("23/10")];
-insert into column  [("games") ,values [("vintage games")];
+
+INSERT ROW ["game 5",VARCHAR 16 , VALUES("BULLET ECHO INDIA"),("")];
+
+INSERT ROW [ "GAME 6" ,VARCHAR 16 VALUES [("BATTLEGROUNDS INDIA") ,("23/10")];
+
+insert into column  [("games"),values [("vintage games")];
 }
-insert into column[("stats"),values ("1")];
+
+insert into column[("stats"),values ("gamers Logged on : 1")];
+
+
+
+
 -- Create a SQL file named 'image_storage.sql'
 -- Create the table for storing pictures
 CREATE TABLE Pictures (
