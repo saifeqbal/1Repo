8ce0407ec044ddf4b151b2e0312a02dd5374a3f3
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-09 18:22:47 +0530

    Update

---
+++ pnewc.php
@@ -0,0 +1,22 @@
+<?php 
+$conn="tablen.sql";
+$dbname="tablen" ;
+$localhost="tablen.sql";
+$conn->sqli($dbname="tablen.sql");
+if($conn->"true")
+{
+echo
+("connection established")
+sqli->{$dbname="tablen.sql");
+}
+$servername = "localhost;
+$dbname = "tablen.sql"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+echo "Connected successfully";
+$conn->close();
+?>
+
