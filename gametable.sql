561753c3978325e5e0f7b80d0b9ffdd758314e9f
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-12 20:57:43 +0530

    Update gametable.sql

--- gametable.sql
+++ gametable.sql
@@ -3,8 +3,9 @@ create table gametableusers;
 //
 insert column ["wman", varchar 16 values ("ev"),("jeva"),("lubt"), ko];
 insert column ["wshapeman " varchar 32 values ("character") , ("values") ,("figure") ("sature")];
-insert row("kobl") ,(" wanb ") , (" vct"), ]
+insert row[("kobl"),(" wanb "),("vct") , varchar ("64"), values ("37","45","26")];
+insert row ("amia",varchar("45") , ("32"), ];
+insert row[("cifa" varchar("53"),values  ("43,"28","32")];
+insert row [("clothes"), ("cholor") ("shoes")varchar ("32"), values("shirt")];
 
-insert row ("45","32");
-insert row[("cifa" varchar("53"),values ("43,"28","32")];
 
