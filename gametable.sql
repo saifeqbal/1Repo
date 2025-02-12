4d72478aad05fc56708c3516fe115ec98f38ac9f
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-12 14:48:15 +0530

    Update

---
+++ gametable.sql
@@ -0,0 +1,7 @@
+create database gametableusers;
+create table gametableusers;
+//
+insert column ["wman", varchar 16 values ("ev"),("jeva"),("lubt"), ko];
+insert column ["wshapeman " varchar 32 values ("character") , ("values") ,("figure") ("sature")];
+insert row("kobl") ," wanb " , ("")
+insert row (""
