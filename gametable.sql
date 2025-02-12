ea3ca91eb4fb56eac9e8630bdca7a3eca73e15a3
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-12 20:02:21 +0530

    Update gametable.sql

--- gametable.sql
+++ gametable.sql
@@ -3,5 +3,8 @@ create table gametableusers;
 //
 insert column ["wman", varchar 16 values ("ev"),("jeva"),("lubt"), ko];
 insert column ["wshapeman " varchar 32 values ("character") , ("values") ,("figure") ("sature")];
-insert row("kobl") ," wanb " , ("")
-insert row (""
+insert row("kobl") ,(" wanb ") , (" vct"), ]
+
+insert row ("45","32");
+insert row[("cifa" varchar("53"),values ("43,"28","32")];
+
