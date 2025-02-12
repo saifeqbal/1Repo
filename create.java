8009715208498914683c816fd37cbe7d49cbe35a
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-12 21:25:33 +0530

    Update create.java

--- create.java
+++ create.java
@@ -2,23 +2,50 @@ import java.io.*;
 import java.awt.*;
 import java.io.*;
 import java.lang.html;
-import java.sql.connction
+import java.sql.connction;
 {
 import SQL.connection;
 import java.connection.sql;
-import 
+import java.sql.connection;
 username
 {
-"saif", "eqbal", 
+"saif","eqbal",
 "java code", 
 };
 login : {"nousename"};
 password: {"#1alightning"}
 }
-if(  login= *"saif " )
+if(login= "saif ")
 {
 System.out.println("login");
 }
+ ("");
 connection{
 }
 else {return 0};
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/tablen.sql";
+        String user = "yourusername";
+        String password = "yourpassword";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            if (conn != null) 
+{
+                System.out.println("Connected to the database!");
+            }
+        } 
+catch (SQLException e)
+ {
+            System.out.println("An error occurred. Maybe user/password is invalid");
+            e.printStackTrace();
+        }
+    }
+}
