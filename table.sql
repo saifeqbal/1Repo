77988b8733ddb1502cf4cd3cc61ff8a72040331c
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-13 16:15:26 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,6 +1,7 @@
 create database tablen;
 create table tablen
 {
+
 insert column ["gamers table", 
 varchar(" 256") ,where values ("gamers table"),(" game starts") ("scores")  ("players")];
 
@@ -8,7 +9,6 @@ insert column[ "title" , varchar ("32") values [(" OS, Android" )];
 
  insert column ("games") VARCHAR 64 WHERE values [ ("Street fighter"),("tekken"),(" Ronin"),("call of dragons"),("OS, Android")]; 
 insert column ["Release" varchar("54"),values( ("12/54")("12,24")];
-
 INSERT INTO COLUMN [("Release")];
 
 {
@@ -18,17 +18,11 @@ insert row("GAME 1", (32) values ("Street fighter")];
 INSERT ROW ["GAME 2" VARCHAR 16 VALUES("call of dragons")];
 INSERT row[ "game 3",VARCHAR 16, values("Galaxiaga")];
 insert row ["game 4",VARCHAR 16 , VALUES("MECH ARENA");
-
 INSERT ROW ["game 5",VARCHAR 16 , VALUES("BULLET ECHO INDIA"),("")];
-
 INSERT ROW [ "GAME 6" ,VARCHAR 16 VALUES [("BATTLEGROUNDS INDIA") ,("23/10")];
-
 insert into column  [("games"),values [("vintage games")];
 }
-
 insert into column[("stats"),values ("gamers Logged on : 1")];
-
-
 
 
 -- Create a SQL file named 'image_storage.sql'
