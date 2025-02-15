bf0918d5b6b1d0c5655b45f2079b13c4cb4d69c7
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-15 14:45:43 +0530

    Update gametable.sql

--- gametable.sql
+++ gametable.sql
@@ -7,5 +7,5 @@ insert row[("kobl"),(" wanb "),("vct") , varchar ("64"),values ("37","45","26")]
 insert row ("amia",varchar("16") , ("32"), ];
 insert row[("cifa" varchar("64"),values  ("43", "28","32")];
 insert row [("clothes"),("cholor"), ("shoes"), varchar ("32"),values("shirt")];
-insert row [("ac"),("fu"), varchar("16") values(""),("70")];
+insert row [("ac"),("fu"), varchar("16") values("a"),("70")];
 
