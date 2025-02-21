1e3660fcdc42392ae2a7bcc932115e01bcb4afa4
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-02-21 16:37:46 +0530

    Update

---
+++ createnewhtml.php
@@ -0,0 +1,33 @@
+ 		<?php
+header('Content-Type: application/json');
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "gamestat.html";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, age, occupation FROM your_table";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0)
+ {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+} else {
+    echo json_encode([]);
+    exit;
+}
+$conn->close();
+echo json_encode($data);
+?>
