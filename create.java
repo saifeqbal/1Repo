3aa2ea37db81e6be163f13cea984505658aeb3c0
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-04-14 09:10:18 +0530

    Update create.java

--- create.java
+++ create.java
@@ -43,10 +43,10 @@ public class DatabaseConnection
                 System.out.println("Connected to the database!");
             }
         } 
-
 catch (SQLException e)
+
  {
-            System.out.println("An error occurred. Maybe user/password is invalid");
+     System.out.println("An error occurred. Maybe user/password is invalid");
             e.printStackTrace();
         }
     }
@@ -59,7 +59,11 @@ public class Main
 }
 
 import java.io.*;
-
+{
+switch 
+{
+}
+sna
 http://maven.apache.org/POM/4.0.0<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
     <modelVersion>4.0.0</modelVersion>
