5473042298a9d429b778a83b71e77dee84c37a7e
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-04-14 14:31:26 +0530

    Update create.java

--- create.java
+++ create.java
@@ -55,15 +55,15 @@ public class Main
 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
+ }
 }
 
 import java.io.*;
 {
-switch 
+switch ( )
 {
 }
-sna
+
 http://maven.apache.org/POM/4.0.0<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
     <modelVersion>4.0.0</modelVersion>
