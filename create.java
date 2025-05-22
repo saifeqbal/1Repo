0f901c3db3851a46db28633fdaab20029f2ecd00
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2025-05-22 18:09:01 +0530

    Update create.java

--- create.java
+++ create.java
@@ -23,61 +23,61 @@ System.out.println("login");
 connection
 {
 }
+
 else {return 0};
+}
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class DatabaseConnection
  {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+ {
         String url = "jdbc:mysql://localhost:3306/tablen.sql";
         String user = "yourusername";
-        String password = "yourpassword";
-
-        try {
+        String password ="yourpassword" ;
+        try 
+{
             Connection conn = DriverManager.getConnection(url, user, password);
             if (conn != null) 
 {
                 System.out.println("Connected to the database!");
-            }
-        } 
+    }
+  
+} 
 catch (SQLException e)
 
- {
+{
      System.out.println("An error occurred. Maybe user/password is invalid");
             e.printStackTrace();
         }
     }
 }
+
 public class Main 
 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
  }
 }
-
 import java.io.*;
 {
 switch ( )
 {
 case :
 }
-
-http://maven.apache.org/POM/4.0.0<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+http://maven.apache.org/POM/4.0.0<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
     <modelVersion>4.0.0</modelVersion>
-
     <groupId>com.example</groupId>
     <artifactId>java-application</artifactId>
     <version>1.0-SNAPSHOT</version>
-
     <properties>
-        <maven.compiler.source>17</maven.compiler.source>
+   <maven.compiler.source>17</maven.compiler.source>
         <maven.compiler.target>17</maven.compiler.target>
     </properties>
     <dependencies>
         <!-- Add dependencies here -->
     </dependencies>
+
 </project>
